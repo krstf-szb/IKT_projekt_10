b0ac5e49c7a1ea532505829d7370d6e9a258b440
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-09-20 16:51:58 +0200

    Add files via upload

---
+++ Recept_02.py
@@ -0,0 +1,9 @@
+def sajtkrémesparizeresszendvics(cheese_cream, topping, spice):
+    print('Fogjunk 2 szelet kenyeret')
+    print('Kenjük meg az eggyik szeletet ' + cheese_cream + '-el')
+    print('Rakjunk rá 2-3 szelet ' + topping + '-t')
+    print('Locsoljuk meg ' + spice + '-al')
+    print('Csappassuk rá a másik szeélet kenyeret')
+    print('Kész, lehet enni')
+
+sajtkrémesparizeresszendvics('füstölt sajtkrém', 'sajtos parizer', 'ketchup')
