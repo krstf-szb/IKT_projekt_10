06a3698185fa947717b4ada7d6ad703a29382334
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-10-01 13:05:47 +0200

    hazi

---
+++ 4adat.py
@@ -0,0 +1,7 @@
+alkatresz = input('Mihez kell neked alkatrész cula?: ')
+alkatresz_fajtaja = input('Milyen fajtályú alkatrész kell?: ')
+szazalek = float(input('Mennyi a drop chance?: '))
+gyartasi_ido = int(input('Mennyi idő még legyártódik a teljes cucc?: '))
+
+
+print ('Neked ' + alkatresz + '-hoz, kell ' + alkatresz_fajtaja + ', aminek, ' ,szazalek, '%' 'eselye van, hogy kiessen, és ' ,gyartasi_ido, 'óra míg legyártódik')
