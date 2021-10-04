1885c665cfee757b37380edb918bb454b155ecbb
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-10-04 10:35:48 +0200

    Add files via upload
    if, elif, else

---
+++ feltetel.py
@@ -0,0 +1,9 @@
+a = int(input('Add meg az egyik számot: '))
+b = int(input('Add meg a második számot: '))
+
+if (a > b):
+    print('Az első szám nagyobb.')
+elif(a == b):
+    print('A két szám egyenlő.')
+else:
+    print('A második szám nagyobb.')
