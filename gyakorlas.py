315eb9df92dcb6a2ecfad5c3ff544270c4614ca1
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-10-04 10:18:26 +0200

    Add files via upload
    hétfői ikt gyakorlás

---
+++ gyakorlas.py
@@ -0,0 +1,54 @@
+print('1. feladat')
+
+print('Szia ' + input('Mi a neved cula?: ')+ '!')
+
+
+print('2. feladat')
+
+print('**********')
+
+print('oooooooooo')
+
+
+print('3. feladat')
+
+print('*o*o*o*o*o')
+
+print('*o*o*o*o*o')
+
+
+print('4. feladat, összeadás')
+
+a = int(input('Adj meg egy számot: '))
+b = int(input('Adj meg még egy számot: '))
+print('A 2 szám összege: ' , a+b ,)
+
+
+print('5. feladat, összeadás 2')
+
+a = int(input('Adj meg egy számot: '))
+b = int(input('Adj meg még egy számot: '))
+c = int(input('Adj meg még egy harmadik számot is: '))
+print('A 3 szám összege: ' , a+b+c ,)
+
+
+print('6. feladat, kivonás')
+
+a = int(input('Adj meg egy számot: '))
+b = int(input('Adj meg még egy számot: '))
+print('A 2 szám külömbsége: ' , a-b ,)
+
+
+print('7. feladat, szorzás')
+
+a = int(input('Adj meg egy számot: '))
+b = int(input('Adj meg még egy számot: '))
+print('A 2 szám szorzata: ' , a*b ,)
+
+
+print('8. feladat, osztás')
+
+a = int(input('Adj meg egy számot: '))
+b = int(input('Adj meg még egy számot: '))
+c = float(a/b)
+print('A 2 szám hányadosa: ' ,c,)
