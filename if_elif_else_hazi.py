7d1e98aa1c1f539ab1931a879c66afa6e548d9f0
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-10-06 14:09:11 +0200

    Update if_elif_else_hazi.py

--- if_elif_else_hazi.py
+++ if_elif_else_hazi.py
@@ -2,7 +2,7 @@ height = int(input('Add meg a magasságodat cm-ben: '))
 
 if (height < 170):
     print('Törpe vagy cula.')
-elif(height == 170,180):
-    print('Átlagos magas vagy culu.')
-else:
+elif(height > 180):
     print('Gyó cula zsiráf vagy!')
+else:
+    print('Átlagos magas vagy culu.')
