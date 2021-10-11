2a6acb3f9c8d2af65be02e1933e415b5afed5255
krstf-szb <90600962+krstf-szb@users.noreply.github.com>
2021-10-11 10:31:38 +0200

    Add files via upload

---
+++ feltetel_tartomannyal.py
@@ -0,0 +1,30 @@
+print('1. feladat')
+
+x = int(input('Adjon meg egy számot: '))
+
+y = int(input('Adjon meg egy másik számot: '))
+
+z = int(input('Adjon meg egy harmadik számot: '))
+
+a = min(x,y,z)
+print('A legkissebb szám a: ', a ,)
+
+
+
+print('2. feladat')
+
+x = int(input('Adjon meg egy számot: '))
+
+y = int(input('Adjon meg egy másik számot: '))
+
+z = int(input('Adjon meg egy harmadik számot: '))
+
+a = max(x,y,z)
+print('A legnagyobb szám a: ', a ,)
+
+
+
+print('3.feladat')
+
+print('add meg a dogád pontszámát! ')
+x = int(input('> '))
